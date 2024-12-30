ddf5ce599f11b3d189153c4565cb070182c39fab
elisamardiyani0 <141213160+elisamardiyani0@users.noreply.github.com>
2024-12-30 15:47:39 +0700

    Add files via upload

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,37 +1,74 @@
 <template>
-  <div class="container-fluid bg-dark text-light">
-    <div class="row">
-      <div class="col-4">
-        <h3>HUBUNGI KAMI</h3>
-        <H5>SMKN4 TASIKMALAYA</H5>
-        <P>Jl Depok kel. Sukamenak Kec.Purbaratu Kota Tasikmalaya 46196</P>
-        <p>+62265312059</p>
-        <p>info@smkn4-tsm.sch.id</p>
+  <!-- Remove the container if you want to extend the Footer to full width. -->
+  <div>
+    <!-- Footer -->
+    <footer class="text-center text-lg-start text-white" style="background-color: #929fba">
+      <!-- Grid container -->
+      <div class="container p-4 pb-0">
+        <!-- Section: Links -->
+        <section class="">
+          <!--Grid row-->
+          <div class="row">
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 class="text-uppercase mb-4 font-weight-bold">Hubungi kami</h6>
+              <p>Jl Depok Kel. Sukamenak Kec. Purbaratu Kota Tasikmalaya 46196.</p>
+              <p>+62265312059</p>
+              <p>Info@smkn4-tsm.sch.id</p>
+            </div>
+            <!-- Grid column -->
+
+            <hr class="w-100 clearfix d-md-none" />
+
+            <hr class="w-100 clearfix d-md-none" />
+
+            <!-- Grid column -->
+            <hr class="w-100 clearfix d-md-none" />
+
+            <!-- Grid column -->
+            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+              <h6 class="text-uppercase mb-4 font-weight-bold">Link Eksternal</h6>
+              <p><i class="fas fa-home mr-3"></i> Kemdikbud</p>
+              <p><i class="fas fa-envelope mr-3"></i> Dapodikdasmen</p>
+              <p><i class="fas fa-phone mr-3"></i> PSMK</p>
+              <p><i class="fas fa-print mr-3"></i> Disdikjabar</p>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+              <h6 class="text-uppercase mb-4 font-weight-bold">Follow us</h6>
+
+              <!-- Facebook -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #3b5998" href="#!" role="button"><i class="bi bi-facebook"></i></a>
+
+              <!-- Twitter -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #55acee" href="#!" role="button"><i class="bi bi-twitter"></i></a>
+
+              <!-- Google -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #dd4b39" href="#!" role="button"><i class="bi bi-instagram"></i></a>
+
+              <!-- Instagram -->
+              <a class="btn btn-primary btn-floating m-1" style="background-color: #ac2bac" href="#!" role="button"><i class="bi bi-tiktok"></i></a>
+
+              <!-- Linkedin -->
+              <!-- Github -->
+            </div>
+          </div>
+          <!--Grid row-->
+        </section>
+        <!-- Section: Links -->
       </div>
-      <div class="col-4">
-        <h3>MEDIA SOCIAL</h3>
-        <P>Facebook</P>
-        <p>Instagram</p>
-        <p>Youtube</p>
-        <p>Twiter</p>
+      <!-- Grid container -->
+
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+        © 2020 Copyright:
+        <a class="text-white" href="https://mdbootstrap.com/">smkn4tasikmalaya</a>
       </div>
-      <div class="col-4">
-        <h3>Link Eksternal</h3>
-        <p>Kemendikbud</p>
-        <p>Disdik</p>
-      </div>
-    </div>
+      <!-- Copyright -->
+    </footer>
+    <!-- Footer -->
   </div>
-  <div class="container-fluid bg-secondary text-dark">
-    <div class="text-center">
-      <h6>
-        SMKN4 Tasikmalaya.ALL right reserved.Designed and Engineered by elisa
-      </h6>
-    </div>
-  </div>
+  <!-- End of .container -->
 </template>
-<style scoped>
-.container-fluid {
-  padding-top: 3%;
-}
-</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,25 +1,18 @@
 <template>
-  <nav
-    class="navbar navbar-expand-lg bg-secondary sticky-top"
-    style="width: 100%">
+  <nav class="navbar navbar-expand-lg bg-secondary sticky-top" style="width: 100%">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="~assets/img/logo.png" alt="logo" width="50" />
-      </a>
-      <div
-        class="justify-content-end collapse navbar-collapse"
-        id="navbarNavDropdown">
+      <a class="navbar-brand" href="#"> <img src="~assets/img/logo.png" alt="logo" width="70" /> </a>
+      <h3 class="text-white">SMK Negri 4 Tasikmalaya</h3>
+      <div class="justify-content-end collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav">
           <nuxt-link to="/">
             <li class="nav-item">
-              <div class="nav-link active" aria-current="page" href="#">
-                Beranda
-              </div>
+              <h4 class="nav-link active" aria-current="page" href="#">Beranda</h4>
             </li>
           </nuxt-link>
           <nuxt-link to="/profil">
             <li class="nav-item">
-              <div class="nav-link" href="#">Profil</div>
+              <h4 class="nav-link" href="#">Profil</h4>
             </li>
           </nuxt-link>
           <nuxt-link to="/galeri">
